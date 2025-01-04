fd04ab38f061839d6aec1c11b3ab344f054ad5ff
Higor Rocha <higor_36rocha@hotmail.com>
2025-01-04 11:30:29 -0300

    Create ExperienceSection.jsx

---
+++ src/app/components/ExperienceSection.jsx
@@ -0,0 +1,8 @@
+export default function ExperienceSection() {
+  return (
+    <div className="flex flex-col items-center gap-10 text-white my-10">
+      <h1 className="text-4xl font-bold">Experiência</h1>
+      
+    </div>
+  );
+}
