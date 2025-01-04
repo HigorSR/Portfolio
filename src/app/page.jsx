34dce0a2aac3899c8970bbc69b793cd456f1e6b6
Higor Rocha <higor_36rocha@hotmail.com>
2025-01-04 11:30:59 -0300

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,18 @@
+import AboutSection from "./components/AboutSection";
+import ExperienceSection from "./components/ExperienceSection";
 import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
+import ProjectsSection from "./components/ProjectsSection";
 
 export default function Home() {
   return (
     <main className="flex flex-col min-h-screen bg-[#121212]">
-      <div className="container mx-auto px-12 py-4">
+      <div className="container mx-auto px-12">
+        <Navbar />
         <HeroSection />
+        <AboutSection />
+        <ProjectsSection />
+        <ExperienceSection />
       </div>
     </main>
   );
