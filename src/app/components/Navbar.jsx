de80b00f44cb1fd6b4e2f182dbc6e203dde3dd62
Higor Rocha <higor_36rocha@hotmail.com>
2025-01-04 11:30:33 -0300

    Update Navbar.jsx

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -3,7 +3,7 @@ export default function Navbar() {
     <div className="relative">
       <nav className="text-white">
         <p className="text-2xl py-4 mt-2 absolute">higor.dev</p>
-        <div className="flex gap-5 md:visible absolute right-0 mt-2">
+        <div className="flex gap-5 absolute right-0 mt-2">
           <p className="hidden md:inline text-lg px-1 py-1 hover:rounded-full hover:bg-gradient-to-br hover:from-orange-600 hover:to-purple-600">
             <span className="block px-4 py-3 bg-[#121212] rounded-full">
               Sobre
