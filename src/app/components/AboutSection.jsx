45f7651b12ef7601763e4e0f1cb5eb83bf8d98b5
Higor Rocha <higor_36rocha@hotmail.com>
2025-01-06 16:43:34 -0300

    Update AboutSection.jsx

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -37,32 +37,59 @@ const sobreItems = [
 
 const AboutSection = () => {
   return (
-    <section className="text-white" id="about">
-      <div className="flex flex-col md:flex-row py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <div className="">
-          <Image
-            src="/images/about-image.jpg"
-            alt="a"
-            width={500}
-            height={500}
-          />
+    <div className="flex flex-col sm:items-center md:flex-row md:justify-between gap-10 my-20 md:h-screen md:my-0 text-white">
+      <div className="rounded-b-2xl md:rounded-r-2xl md:rounded-bl-none overflow-hidden">
+        <Image
+          src="/images/about-image.jpg"
+          alt="programming"
+          width={700}
+          height={700}
+        />
+      </div>
+      <div className="md:w-1/2 ">
+        <h2 className="text-4xl font-bold text-white mb-4">Sobre Mim</h2>
+        <p className="text-base lg:text-lg mb-4">
+          Sou um desenvolvedor web apaixonado por criar aplicações web
+          interativas e responsivas. Tenho experiência em trabalhar com
+          JavaScript, React, Redux, Node.js, Express, PostgreSQL, HTML, CSS e
+          Git. Aprendo rápido e estou sempre em busca de expandir meus
+          conhecimentos e habilidades. Trabalho em equipe e estou animado para
+          trabalhar com outras pessoas para criar aplicativos incríveis.
+        </p>
+        <div className="collapse bg-base-200 mb-2">
+          <input type="radio" name="my-accordion-1" defaultChecked />
+          <div className="collapse-title text-xl font-medium">Habilidades</div>
+          <div className="collapse-content">
+            <ul>
+              <li>React</li>
+              <li>Next</li>
+              <li>JavaScript</li>
+              <li>Tailwind</li>
+              <li>Node.js</li>
+              <li>API</li>
+            </ul>
+          </div>
         </div>
-        <div className="w-1/2 mt-4 md:mt-0 text-left flex flex-col h-full">
-          <h2 className="text-4xl font-bold text-white mb-4">Sobre Mim</h2>
-          <p className="text-base lg:text-lg">
-            Sou um desenvolvedor web apaixonado por criar aplicações web
-            interativas e responsivas. Tenho experiência em trabalhar com
-            JavaScript, React, Redux, Node.js, Express, PostgreSQL, HTML, CSS e
-            Git. Aprendo rápido e estou sempre em busca de expandir meus
-            conhecimentos e habilidades. Trabalho em equipe e estou animado para
-            trabalhar com outras pessoas para criar aplicativos incríveis.
-          </p>
-          <div>
-            {/* área com as listas */}
+        <div className="collapse bg-base-200 mb-2">
+          <input type="radio" name="my-accordion-1" />
+          <div className="collapse-title text-xl font-medium">Ensino</div>
+          <div className="collapse-content">
+            <ul>
+              <li>Superior em: Ciência da Computação - CEUB</li>
+            </ul>
+          </div>
+        </div>
+        <div className="collapse bg-base-200 mb-2">
+          <input type="radio" name="my-accordion-1" />
+          <div className="collapse-title text-xl font-medium">Certificados</div>
+          <div className="collapse-content">
+            <ul>
+              <li>Em breve...</li>
+            </ul>
           </div>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 
