b05fcfc5916ad7675d62cd3a3d14c051b57abc7c
Higor Rocha <higor_36rocha@hotmail.com>
2025-01-10 12:29:47 -0300

    Update ProjectsSection.jsx

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -1,19 +1,19 @@
 "use client";
 
-import ProjectCard from "./ProjectCar";
+import ProjectCard from "./ProjectCard";
 
 export default function ProjectsSection() {
   const projects = [
     {
       id: 1,
-      title: "a",
-      img: "/images/about-image.jpg",
-      code: "code",
-      live: "live",
+      title: "Intro Component With Sign Up Form",
+      img: "/images/intro-component-with-sign-up-form.png",
+      code: "https://github.com/HigorSR/intro-component-with-sign-up-form",
+      live: "https://higorsr.github.io/intro-component-with-sign-up-form/",
     },
     {
       id: 2,
-      title: "b",
+      title: "Projeto 02",
       img: "/images/about-image.jpg",
     },
     {
@@ -39,14 +39,26 @@ export default function ProjectsSection() {
   ];
 
   return (
-    <div className="flex flex-col items-center gap-10 text-white my-10">
+    <section id="Projetos" className="flex flex-col items-center gap-10 text-white my-10 md:pt-10">
       <h1 className="text-4xl font-bold">Projetos</h1>
       <div className="flex gap-8">
-        <button>Tudo</button>
-        <button>Web</button>
-        <button>Mobile</button>
+        <div className="hidden md:inline text-lg px-1 py-1 hover:rounded-full hover:bg-gradient-to-br hover:from-orange-600 hover:to-purple-600">
+          <button className="block px-4 py-3 bg-[#242424] rounded-full">
+            Tudo
+          </button>
+        </div>
+        <div className="hidden md:inline text-lg px-1 py-1 hover:rounded-full hover:bg-gradient-to-br hover:from-orange-600 hover:to-purple-600">
+          <button className="block px-4 py-3 bg-[#242424] rounded-full">
+            Web
+          </button>
+        </div>
+        <div className="hidden md:inline text-lg px-1 py-1 hover:rounded-full hover:bg-gradient-to-br hover:from-orange-600 hover:to-purple-600">
+          <button className="block px-4 py-3 bg-[#242424] rounded-full">
+            Mobile
+          </button>
+        </div>
       </div>
-      <div className="flex flex-wrap items-center justify-center gap-5">
+      <div className="flex flex-wrap items-center justify-center gap-x-7 md:gap-x-12 gap-y-8 sm:gap-y-12">
         {projects.map((project, index) => (
           <ProjectCard
             key={index}
@@ -57,6 +69,6 @@ export default function ProjectsSection() {
           />
         ))}
       </div>
-    </div>
+    </section>
   );
 }
