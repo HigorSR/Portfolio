5c5f01a2a9fb7df6b5ae165136033d3b2900a9be
Higor Rocha <higor_36rocha@hotmail.com>
2025-01-10 12:29:53 -0300

    Update ExperienceSection.jsx

--- src/app/components/ExperienceSection.jsx
+++ src/app/components/ExperienceSection.jsx
@@ -1,8 +1,18 @@
+import WavingHandTwoToneIcon from "@mui/icons-material/WavingHandTwoTone";
+
 export default function ExperienceSection() {
   return (
-    <div className="flex flex-col items-center gap-10 text-white my-10">
-      <h1 className="text-4xl font-bold">Experiência</h1>
-      
-    </div>
+    <section id="Experiencia" className="flex flex-col items-center gap-10 text-white my-10">
+        <h1 className="text-4xl font-bold">Experiência</h1>
+      <div className="flex flex-col items-center gap-2">
+        <p className="text-center">Em busca da primeira oportunidade de emprego!</p>
+        <WavingHandTwoToneIcon/>
+      </div>
+      <div>
+        <p>.</p>
+        <p>.</p>
+        <p>.</p>
+      </div>
+    </section>
   );
 }
