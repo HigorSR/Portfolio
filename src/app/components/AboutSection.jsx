be65e233712fae3a6553e9ec12a9dd84b9e420bf
Higor Rocha <higor_36rocha@hotmail.com>
2025-03-25 17:46:29 -0300

    Update AboutSection.jsx
    New skills container

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -37,7 +37,10 @@ const sobreItems = [
 
 const AboutSection = () => {
   return (
-    <div id="Sobre" className="flex flex-col sm:items-center lg:flex-row md:justify-between gap-10 my-20 lg:h-screen md:my-0 text-white">
+    <div
+      id="Sobre"
+      className="flex flex-col sm:items-center lg:items-start lg:flex-row md:justify-between gap-10 my-20 lg:h-screen md:my-0 text-white lg:pt-40"
+    >
       <div className="rounded-b-2xl lg:rounded-r-2xl lg:rounded-bl-none overflow-hidden md:w-[500px] lg:w-[700px]">
         <Image
           src="/images/about-image.jpg"
@@ -58,11 +61,55 @@ const AboutSection = () => {
             trabalhar com outras pessoas para criar aplicativos incríveis.
           </p>
         </div>
-        <div className="collapse bg-base-200 mb-2">
+        <div className="tabs tabs-lifted">
+          <input
+            type="radio"
+            name="my_tabs_2"
+            className="tab text-white text-base"
+            aria-label="Habilidades"
+            defaultChecked
+          />
+          <div className="tab-content bg-base-100 rounded-2xl border-base-300 p-6">
+            <ul className="list-disc list-inside">
+              <li>React</li>
+              <li>Next</li>
+              <li>JavaScript</li>
+              <li>Tailwind</li>
+              <li>Node.js</li>
+              <li>API</li>
+            </ul>
+          </div>
+
+          <input
+            type="radio"
+            name="my_tabs_2"
+            className="tab text-white"
+            aria-label="Ensino"
+          />
+          <div className="tab-content bg-base-100 p-6">
+            <ul className="list-disc list-inside">
+              <li>Superior em: Ciência da Computação - CEUB</li>
+            </ul>
+          </div>
+
+          <input
+            type="radio"
+            name="my_tabs_2"
+            className="tab text-white"
+            aria-label="Certificados"
+          />
+          <div className="tab-content bg-base-100 p-6">
+            <ul className="list-disc list-inside">
+              <li>Em breve...</li>
+            </ul>
+          </div>
+        </div>
+        
+        {/* <div className="collapse collapse-arrow bg-base-200 mb-2 px-1 py-1 bg-gradient-to-br from-orange-600 to-purple-600 hover:bg-slate-800 text-white mt-3">
           <input type="radio" name="my-accordion-1" defaultChecked />
           <div className="collapse-title text-xl font-medium">Habilidades</div>
           <div className="collapse-content">
-            <ul>
+            <ul className="list-disc list-inside">
               <li>React</li>
               <li>Next</li>
               <li>JavaScript</li>
@@ -72,24 +119,24 @@ const AboutSection = () => {
             </ul>
           </div>
         </div>
-        <div className="collapse bg-base-200 mb-2">
+        <div className="collapse collapse-arrow bg-base-200 mb-2 px-1 py-1 bg-gradient-to-br from-orange-600 to-purple-600 hover:bg-slate-800 text-white mt-3">
           <input type="radio" name="my-accordion-1" />
           <div className="collapse-title text-xl font-medium">Ensino</div>
           <div className="collapse-content">
-            <ul>
+            <ul className="list-disc list-inside">
               <li>Superior em: Ciência da Computação - CEUB</li>
             </ul>
           </div>
         </div>
-        <div className="collapse bg-base-200 mb-2">
+        <div className="collapse collapse-arrow bg-base-200 mb-2 px-1 py-1 bg-gradient-to-br from-orange-600 to-purple-600 hover:bg-slate-800 text-white mt-3">
           <input type="radio" name="my-accordion-1" />
           <div className="collapse-title text-xl font-medium">Certificados</div>
           <div className="collapse-content">
-            <ul>
+            <ul className="list-disc list-inside">
               <li>Em breve...</li>
             </ul>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
